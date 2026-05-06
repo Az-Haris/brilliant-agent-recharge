2c269809c3cdb9ffcb971564ef55efcbf8536c2c
MD Alimuzzaman Haris <mdazharis437@gmail.com>
2026-05-06 22:50:04 +0600

    Updated Hero image

--- components/HeroCarousel.tsx
+++ components/HeroCarousel.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const HeroCarousel = () => {
   return (
-    <div className="relative w-full aspect-21/7 rounded-xl overflow-hidden">
+    <div className="relative w-full aspect-21/9 rounded-b-xl overflow-hidden">
       <Image
         src="/Slider_1.jpg"
         alt="Hero Banner"
